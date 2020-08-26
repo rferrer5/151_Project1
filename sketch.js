c7a154fc6d17b18a3e4623bece4c603b48b30ad8
rferrer5 <70284164+rferrer5@users.noreply.github.com>
2020-08-26 14:24:32 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 let x = 0;
-let speed = 3;
+let y = 50;
+let speedX = 30;
+let speedY = 3;
+let angle = 0.0;
 
 function setup() {
   createCanvas(400,400);
@@ -12,11 +15,31 @@ function draw() {
   rect(x,100,100,100);
 
   fill(0,0,255);
-  ellipse(300,300,100,100);
-  
-  if (x + 100 > width) {
-  	speed = (-1*speed);
+  ellipse(300,y,100,100);
+
+  if (second() % 2 == 0) {
+	angle = angle + 1.0;
   }
 
-  x = x + speed;
+  fill (255,0,0);
+  rect(50,250,100,100);
+  
+  if (x + 100 > width) {
+  	speedX = (-1*speedX);
+  }
+
+  if (y + 50 > height) {
+	speedY = (-1*speedY);
+  }
+
+  x = x + speedX;
+  y = y + speedY;  
+
+  if (x == 0) {
+	speedX = (-1*speedX);
+  }
+
+  if (y == 50) {
+	speedY = (-1*speedY);
+  }
 }

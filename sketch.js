8525adf517458a49507fb7c325d3b58ff09148ff
rferrer5 <70284164+rferrer5@users.noreply.github.com>
2020-08-31 14:34:47 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,30 @@
-let x = 0;
-let y = 50;
-let speedX = 30;
-let speedY = 3;
-let angle = 0.0;
+let x = 450;
+let speed = 10;
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(windowWidth, windowHeight);
+  background(255, 100, 180);
 }
 
 function draw() {
-  background(100,100,100);
+  ellipse(x, mouseY, mouseY / 2 + 10, mouseY / 2 + 10);
+  stroke(22);
 
-  fill(20,30,100);
-  rect(x,100,100,100);
-
-  fill(0,0,255);
-  ellipse(300,y,100,100);
-
-  if (second() % 2 == 0) {
-	angle = angle + 1.0;
+  if (x + 50 > width || x < 50) {
+    speed = -1 * speed;
   }
+  x = x + speed;
 
-  fill (255,0,0);
-  rect(50,250,100,100);
-  
-  if (x + 100 > width) {
-  	speedX = (-1*speedX);
+  if (mouseIsPressed) {
+    var r = Math.random() * 255 + 1;
+    var g = Math.random() * 255 + 1;
+    var b = Math.random() * 255 + 1;
+    fill(r, g, b);
   }
+}
 
-  if (y + 50 > height) {
-	speedY = (-1*speedY);
-  }
-
-  x = x + speedX;
-  y = y + speedY;  
-
-  if (x == 0) {
-	speedX = (-1*speedX);
-  }
-
-  if (y == 50) {
-	speedY = (-1*speedY);
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    saveCanvas('image/myCanvas', 'png');
   }
 }
